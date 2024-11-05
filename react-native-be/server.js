df5e9336534e6f2787109a5a0490160d35c2b521
Dinh Trang <trangdh7@fpt.com>
2024-11-05 10:22:08 +0700

    update server.js

--- react-native-be/server.js
+++ react-native-be/server.js
@@ -17,14 +17,14 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
-  console.log('Connected to MongoDB');
-}).catch((error) => {
-  console.error('MongoDB connection error:', error);
-});
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('MongoDB connection error:', error);
+  });
+
 
 // Define a sample route
 app.get('/', (req, res) => {
